61d84cdd8cba384999a940bb49297e5d98217645
Your Name <you@example.com>
2023-09-18 17:07:17 -0500

    Fixed access_container file

--- scripts/access_container.sh
+++ scripts/access_container.sh
@@ -1,1 +1,13 @@
-#!/bin/bash# Define the container name or IDCONTAINER_NAME="cobbler_container"# Check if the container is runningif docker ps --format '{{.Names}}' | grep -q "^${CONTAINER_NAME}$"; then    echo "Accessing the container: $CONTAINER_NAME"    docker exec -it $CONTAINER_NAME /bin/bashelse    echo "Error: Container $CONTAINER_NAME is not running."    exit 1fi
+#!/bin/bash
+
+# Define the container name or ID
+CONTAINER_NAME="cobbler_container"
+
+# Check if the container is running
+if docker ps --format '{{.Names}}' | grep -q "^${CONTAINER_NAME}$"; then
+    echo "Accessing the container: $CONTAINER_NAME"
+    docker exec -it $CONTAINER_NAME /bin/bash
+else
+    echo "Error: Container $CONTAINER_NAME is not running."
+    exit 1
+fi
